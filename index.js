fb2f1458073dea3e766886404e612ac88abde8ee
Saad Ali <115668468+Saad-04@users.noreply.github.com>
2023-07-05 03:26:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,89 @@
-// import http from 'http';
-// import { love } from './module.js';
+ import http from 'http';
+ import { love } from './module.js';
 
-// const server  = http.createServer((req,res)=>{
-//         console.log(res.end(`this is ${love()}`))    
-// })
+ const server  = http.createServer((req,res)=>{
+         console.log(res.end(`this is ${love()}`))    
+ })
 
-// server.listen(5000,()=>{
-// console.log("my name is saad")
-// })
+ server.listen(5000,()=>{
+ console.log("my name is saad")
+ })
 
-// const app = express()
-// mongoose.connect("mongodb://127.0.0.1:27017", {
-        //         dbName: "backend",
-        // }).then(() => console.log("Database Connected")).catch((e) => console.log(e));
+ const app = express()
+ mongoose.connect("mongodb://127.0.0.1:27017", {
+                 dbName: "backend",
+         }).then(() => console.log("Database Connected")).catch((e) => console.log(e));
         
-        // // all middlewares
-        // app.set("view engine", "ejs")
-// app.use(express.static(path.join(path.resolve(), 'public')))
-// app.use(express.urlencoded({
-//         extended: true
-// }))
-// app.use(cookieParser())
-// const isAuthenticated = (req,res,next)=>{
-//  const token = req.cookies.token
-//         if (token) {
-//                 next()
-//         } else {
-//                 res.render("login.ejs")
-//         }
-// }
-// app.get('/', isAuthenticated,(req, res) => {
-//         // res.sendStatus(500)}
-//         // res.json({
-//         //         "name": true,
-//         //         "age":23 ,
-//         // })
-//         // const p = path.resolve()
-//         // res.sendFile(path.join(p,'./index.html'))
+        //  all middlewares
+        app.set("view engine", "ejs")
+ app.use(express.static(path.join(path.resolve(), 'public')))
+         app.use(express.urlencoded({
+         extended: true
+ }))
+ app.use(cookieParser())
+ const isAuthenticated = (req,res,next)=>{
+  const token = req.cookies.token
+if (token) {
+next()
+} else {
+                res.render("login.ejs")
+        }
+ }
+ app.get('/', isAuthenticated,(req, res) => {
+         res.sendStatus(500)}
+          res.json({
+                  "name": true,
+                 "age":23 ,
+        })
+          const p = path.resolve()
+         res.sendFile(path.join(p,'./index.html'))
 
-//         res.render('logout')
-// })
-// const userSchema = new mongoose.Schema({
-//         email: String,
-//         password: String,
-// });
+         res.render('logout')
+ })
+      email: String,
+password: String,
+ });
 
-// const Message = mongoose.model("Message", userSchema);
-// // add data in json 
-// // app.get('/user', (req, res) => {
-        // //             res.json(arr) 
-        // //              res.render("user")  
-        // // })
+const Message = mongoose.model("Message", userSchema);
+// add data in json 
+app.get('/user', (req, res) => {
+                     res.json(arr) 
+                     res.render("user")  
+        })
         
-        // app.post('/', async (rq, rs) => {
-                //         // console.log(rq.body.email)
-                //         const {
-                        //                 email,
-                        //                 password
-                        //         } = rq.body
-                        //         const done = await Message.create({
-                                //                 email,
-                                //                 password
-                                //         })
-                                //         if (done) {
-                                        //                 rs.send('done')
-                                        //         }
-                                        // })
-                                        // // login section start here 
-                                        // app.post('/login', (req, res) => {
-                                                //         res.cookie("token", "its work", {
-                                                        //                 httpOnly: true,
-                                                        //                 expires: new Date(Date.now() + 60 * 1000)
-                                                        //         });
-//         res.redirect('/')
-// })
-// // logout section start here 
-// app.get('/logout', (req, res) => {
-//         res.cookie("token", null, {
-//                 expires: new Date(Date.now())
-//         });
-//         res.redirect('/')
-// })
+      app.post('/', async (rq, rs) => {
+                       console.log(rq.body.email)
+                       const {
+                                     email,
+                                       password
+                              } = rq.body
+                              const done = await Message.create({
+                                             email,
+                                              password
+                                        })
+                                     if (done) {
+                                                      rs.send('done')
+                                                }
+                                       })
+                                      // login section start here 
+                                      app.post('/login', (req, res) => {
+                                                      res.cookie("token", "its work", {
+                                                                      httpOnly: true,
+                                                                      expires: new Date(Date.now() + 60 * 1000)
+                                                              });
+         res.redirect('/')
+ })
+ // logout section start here 
+app.get('/logout', (req, res) => {
+         res.cookie("token", null, {
+                 expires: new Date(Date.now())
+       });
+        res.redirect('/')
+})
 
-// app.listen('5000', () => {
-//         console.log('server start')
-// })
+ app.listen('5000', () => {
+        console.log('server start')
+ })
 
 
 
